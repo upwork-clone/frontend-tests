e0fdea8c4503aad51f62ddc1d7efbacb9d4fe72f
GeoXhacker <mukisageophrey@gmail.com>
2023-01-04 11:06:18 +0300

    first commit

---
+++ src/routes/ROUTES.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import RenderRoutes from "./RenderRoutes";
+
+import FreelancerFeed from "pages/Feeds/Freelancer/FreelancerFeed";
+
+
+const ROUTES = [
+	{ path: "/", exact: true, component: FreelancerFeed },
+];
+
+export default ROUTES;
